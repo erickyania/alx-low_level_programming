79a727ada8f6b27ec1c4df06d65090176385ecf0
erickyania <erickyania0@gmail.com>
2022-09-12 10:52:35 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
 *main - print the string in the put function
 *
-* description : using the main function
-* this program prints 
-* this program prints "programming is like building a multilingual puzzle
-* return: 0
-**/
+* description: using the main function
+* this program prints "programming is like building a multilingual puzzle"
+*return: 0
+*/
 int main(void)
 {
-    puts("/"programming is like building a multilingual puzzle");
-    return: (0);
+        puts("\"programming is like building a multilingual puzzle");
+        return:(0);
+}
